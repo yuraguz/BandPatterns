1d360220e665b9aa9479a2aa5243779a0012b924
guz_us <guz_us@prosoftlab.ru>
2016-12-15 09:43:00 +0300

    Hello, patterns project.

---
+++ src/main/java/generating/abstractfactory/App.java
@@ -0,0 +1,10 @@
+package generating.abstractfactory;
+
+/**
+ * Created by guz_us on 15.12.2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

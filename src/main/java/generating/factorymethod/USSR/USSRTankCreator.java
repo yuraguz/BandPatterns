7b89636345a0ccb5eb19d3a4ff57b0c0e7fd51a5
Guz Yury Sergeevich <guz_us@prosoftlab.ru>
2016-12-29 12:59:04 +0300

    commit master

--- src/main/java/generating/factorymethod/USSR/USSRTankCreator.java
+++ src/main/java/generating/factorymethod/USSR/USSRTankCreator.java
@@ -27,8 +27,6 @@ public class USSRTankCreator implements TankCreator {
         }
 
         System.out.println("TEST");
-        System.out.println("It is from TestBranch1");
-        System.out.println("It is from TestBtanch1 too");
 
         return null;
 

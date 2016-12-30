633fb06b9a533d3d7403b2035f301e29c08a2f4d
Guz Yury Sergeevich <guz_us@prosoftlab.ru>
2016-12-30 09:47:59 +0300

    test branch

--- src/main/java/generating/factorymethod/USSR/USSRTankCreator.java
+++ src/main/java/generating/factorymethod/USSR/USSRTankCreator.java
@@ -27,8 +27,7 @@ public class USSRTankCreator implements TankCreator {
         }
 
         System.out.println("My new branch");
-        System.out.println("My new branch");
-        System.out.println("LOL");
+        
 
         return null;
 
